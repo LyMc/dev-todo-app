269ac0630d26414ab03359602bca1039ece5c629
Training13 <Training@Traning13.ptc.local>
2018-02-21 10:37:07 +0200

    course 3, new tasks

---
+++ index.js
@@ -0,0 +1,25 @@
+function log(...rest) {
+  console.log(...rest)
+}
+// tasks 3. Modulo operator
+// function seq(n) {
+//   var s = []
+//   for (var i = 0; i < n; i++) {
+//     s.push(i%2)
+//   }
+//   return s
+// }
+// function seq2(prev) {
+//   return prev = (prev + 1) % 2
+// }
+// function seq3(prev) {
+//   return prev = prev ? 0 : 1
+// }
+// function reverse(n) {
+//   var res = 0
+//   while (n) {
+//     res = res * 10 + n % 10
+//     n = Math.floor(n / 10)
+//   }
+//   return res
+// }
