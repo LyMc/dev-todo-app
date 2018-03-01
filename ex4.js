ef2ed4cdc8b741d798d5a071e6afa80ef768cd2e
Training13 <Training@Traning13.ptc.local>
2018-03-01 12:56:06 +0200

    Added node

---
+++ ex4.js
@@ -0,0 +1,40 @@
+// const url = 'https://jsonplaceholder.typicode.com/posts/1'
+const userURL = 'http://localhost:3000/user'
+function get(url, callback) {
+  var request = new XMLHttpRequest()
+  request.open('GET', url)
+  request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+      var type = request.getResponseHeader('Content-Type')
+      if (type.indexOf('xml') !== -1 && request.responseXML) {
+        callback(request.responseXML)
+      } else if (type.indexOf('json') !== -1) {
+        callback(JSON.parse(request.responseText))
+      } else {
+        callback(request.responseText)
+      }
+    }
+  }
+  request.send(null)
+}
+
+// get(url, console.log)
+// jQuery.ajax({
+//   type: 'GET',
+//   url: url,
+//   data: null,
+//   success: console.log
+// })
+
+function makeAjaxCall(url, type) {
+  return $.ajax({
+    url,
+    type,
+    dataType: 'json'
+  })
+}
+
+// const p = makeAjaxCall(url, 'GET').then(console.log)
+
+makeAjaxCall(userURL, 'GET')
+  .then(resp => name.value = resp.name)
---
+++ express/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/user', (req, res) => res.send({
+  name: 'Victor',
+  email: 'victor@locoman.ro'
+}))
+
+app.listen(3000, () => console.log('Listening...'))
