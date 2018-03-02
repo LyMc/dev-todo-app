4606acc04b8477d8206953f3aacc29fd170de573
Training13 <Training@Traning13.ptc.local>
2018-03-02 10:17:21 +0200

    ex4 finished

--- express/index.js
+++ express/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+var cors = require('cors')
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.use(cors())
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './ex4.html')))
+app.get('/ex4.js', (req, res) => res.sendFile(path.join(__dirname, './ex4.js')))
+app.get('/ex4.css', (req, res) => res.sendFile(path.join(__dirname, './ex4.css')))
 app.get('/user', (req, res) => res.send({
   name: 'Victor',
   email: 'victor@locoman.ro'
